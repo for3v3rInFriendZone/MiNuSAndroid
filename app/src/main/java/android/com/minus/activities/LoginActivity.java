3c76462002b712d6d2499fd4914692acc9715f7e
for3v3rInFriendZone <strile93@gmail.com>
2017-04-09 16:21:20 +0200

    Some minor changes in registerLayout.

--- app/src/main/java/android/com/minus/activities/LoginActivity.java
+++ app/src/main/java/android/com/minus/activities/LoginActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiti_login);
 
