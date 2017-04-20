bd042dec47ffed24b0ce86ce486649875caa01e3
DejanUrosevic <dejansuca@gmail.com>
2017-04-20 17:06:27 +0200

    Added barchart for year report.

--- app/src/main/java/android/com/minus/activities/MainActivity.java
+++ app/src/main/java/android/com/minus/activities/MainActivity.java
@@ -25,6 +25,9 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -51,7 +54,7 @@ public class MainActivity extends AppCompatActivity
     private ImageButton datePicker;
     private FloatingActionButton newBill;
     private View recyclerView;
-    private BarChart chart;
+    private BarChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +73,7 @@ public class MainActivity extends AppCompatActivity
         dayPicker = (TextView) findViewById(R.id.day_picker_report);
         datePicker = (ImageButton) findViewById(R.id.day_date);
         datePicker.setImageResource(R.mipmap.ic_calendar_range);
-        chart = (BarChart) findViewById(R.id.bar_chart);
+        mChart = (BarChart) findViewById(R.id.bar_chart);
         searchInput = (SearchView) findViewById(R.id.search_input);
         searchInput.setQueryHint("Pretraga računa...");
         recyclerView = findViewById(R.id.item_list);
@@ -217,10 +220,6 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setLayoutParams(params);
 
 
-        BarData data = new BarData(getDataSet());
-        chart.setData(data);
-        chart.animateXY(2000, 2000);
-        chart.invalidate();
 
         showDate(month+1, year);
 
@@ -233,34 +232,6 @@ public class MainActivity extends AppCompatActivity
                 pd.show(getFragmentManager(), "MonthYearPickerDialog");
             }
         });
-    }
-
-    private ArrayList<IBarDataSet> getDataSet() {
-        ArrayList<BarEntry> dataSets = new ArrayList<BarEntry>();
-
-        for (int i = (int) 0; i < 30; i++) {
-            float val = (float) (Math.random()*10);
-            dataSets.add(new BarEntry(i, val));
-        }
-
-        BarDataSet barDataSet1 = new BarDataSet(dataSets, "Brand 1");
-        barDataSet1.setColor(Color.rgb(0, 155, 0));
-
-        ArrayList<IBarDataSet> idataSets = new ArrayList<IBarDataSet>();
-        idataSets.add(barDataSet1);
-
-        return idataSets;
-    }
-
-    private ArrayList<String> getXAxisValues() {
-        ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
-        return xAxis;
     }
 
     private void yearReport() {
@@ -282,6 +253,45 @@ public class MainActivity extends AppCompatActivity
                 pd.show(getFragmentManager(), "MonthYearPickerDialog");
             }
         });
+
+        BarData bardata = new BarData(getXAxisValues(),getDataSet());
+      /*  ...............Static Data for Data ListView..............................*/
+        String[] date = new String[]{"21-May-2006",
+                "7-Jan-2016", "27-April-2016"};
+        String[] cardtype = new String[]{
+                "Debit Card",
+                "Credit Card",
+                "Debit Card"};
+        String[] trans_value = {"\u20B945777",
+                "\u20B94500","\u20B93400"};
+
+        mChart.setData(bardata);
+        mChart.setDescription(" ");
+        mChart.animateXY(2000, 3000);
+        mChart.setBackgroundColor(Color.rgb(30,144,255));
+        mChart.setGridBackgroundColor(Color.rgb(149, 27, 75));
+        mChart.setDrawBarShadow(false);
+        mChart.setDrawValueAboveBar(true);
+        mChart.setDescription("");
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.WHITE);
+       /* xAxis.setTypeface(mTf);*/
+        xAxis.setDrawGridLines(false);
+        xAxis.setSpaceBetweenLabels(1);
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setEnabled(false);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+        Legend l = mChart.getLegend();
+        l.setEnabled(false);
+        mChart.invalidate();
     }
 
     private void all_bills() {
@@ -311,6 +321,63 @@ public class MainActivity extends AppCompatActivity
 
     public void showDate(int year) {
         dayPicker.setText(new StringBuilder().append(year));
+    }
+
+    private ArrayList<IBarDataSet> getDataSet() {
+        //ArrayList<BarDataSet> dataSets = null;
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // Maj
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+        BarEntry v1e7 = new BarEntry(110.000f, 6); // Jul
+        valueSet1.add(v1e7);
+        BarEntry v1e8 = new BarEntry(40.000f, 7); // Avg
+        valueSet1.add(v1e8);
+        BarEntry v1e9 = new BarEntry(60.000f, 8); // Sep
+        valueSet1.add(v1e9);
+        BarEntry v1e10 = new BarEntry(30.000f, 9); // Okt
+        valueSet1.add(v1e10);
+        BarEntry v1e11 = new BarEntry(90.000f, 10); // Nov
+        valueSet1.add(v1e11);
+        BarEntry v1e12 = new BarEntry(100.000f, 11); // Dec
+        valueSet1.add(v1e12);
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, " ");
+        barDataSet1.setColor(Color.rgb(255,255,0));
+        barDataSet1.setValueTextColor(Color.WHITE);
+        barDataSet1.setHighLightColor(Color.WHITE);
+        barDataSet1.setValueTextColor(Color.WHITE);
+        barDataSet1.setBarSpacePercent(30f);
+        // dataSets = new ArrayList<>();
+        // dataSets.add(barDataSet1);
+        ArrayList<IBarDataSet> datas = new ArrayList<IBarDataSet>();
+        datas.add(barDataSet1);
+        return datas;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAJ");
+        xAxis.add("JUN");
+        xAxis.add("JUL");
+        xAxis.add("AVG");
+        xAxis.add("SEP");
+        xAxis.add("OKT");
+        xAxis.add("NOV");
+        xAxis.add("DEC");
+        return xAxis;
     }
 
 }
