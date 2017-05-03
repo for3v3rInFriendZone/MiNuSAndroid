5efab49ba7394c928649b02b9908b673204b7408
for3v3rInFriendZone <strile93@gmail.com>
2017-05-03 15:31:56 +0200

    Retrofit2 tryout for posting a new user to remote database.

---
+++ app/src/main/java/DAO/UserDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import java.util.List;
+import model.User;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface UserDAO {
+
+    String BASE_URL = "http://192.168.1.54:8080";
+
+    @GET("/user")
+    Call<List<User>> findAll();
+
+    @POST("/user")
+    Call<ResponseBody> save(@Body User u);
+}
--- app/src/main/java/android/com/minus/activities/RegisterActivity.java
+++ app/src/main/java/android/com/minus/activities/RegisterActivity.java
@@ -4,11 +4,25 @@ import android.com.minus.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.gson.GsonBuilder;
 
-public class RegisterActivity extends AppCompatActivity {
+import DAO.UserDAO;
+import model.User;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RegisterActivity extends AppCompatActivity implements Callback<ResponseBody>{
+
+    private UserDAO userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         Button regButton = (Button)findViewById(R.id.regButton);
+        createUserDAO();
 
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,8 +41,33 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void toLOginPage(View v){
-        Intent i = new Intent(this, LoginActivity.class);
 
+        userDao.save(new User("wwww", "ssss", "aaaaa", "fffff", "bbbbbbbb4")).enqueue(this);
+
+        Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
+    }
+
+    private void createUserDAO() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(UserDAO.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+                .build();
+
+        userDao = retrofit.create(UserDAO.class);
+    }
+
+    @Override
+    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+        if(response.isSuccessful()) {
+            Toast.makeText(RegisterActivity.this, "Upvote successful", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("QuestionsCallback", "Code: " + response.code() + " Message: " + response.message());
+        }
+    }
+
+    @Override
+    public void onFailure(Call<ResponseBody> call, Throwable t) {
+        t.printStackTrace();
     }
 }
